02c44ec8a14249eadf7d7aee723d4d0eb4f3e86f
Uday <udayraj.vadeghar@gmail.com>
2025-03-21 11:50:56 +0000

    Add the files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello from express");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
