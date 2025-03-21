4beebd406d4494ae53808aa69a092068a46f67f2
Uday <udayraj.vadeghar@gmail.com>
2025-03-21 12:08:54 +0000

    test1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello from express and this is success");
+  res.send("hello from express and this is success and lets goo");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
